78e60f9c14bde12ae1b4a6f3c02b697d0ebe3d6f
shtmjp <shtm.1058@gmail.com>
2024-11-07 08:16:40 +0000

    add execution commad

--- src/main.py
+++ src/main.py
@@ -47,3 +47,7 @@ def main() -> None:
         summary = get_summary(result)
         webhook = DiscordWebhook(url=DISCORD_WEBHOOK_URL, content=summary)
         webhook.execute()
+
+
+if __name__ == "__main__":
+    main()
